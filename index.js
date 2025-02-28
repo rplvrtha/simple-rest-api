08c9e39d915fb873a9f03d17b6f1b80ad99e3d71
rplvrtha <radityaloviartha@gmail.com>
2025-02-28 10:17:02 +0700

    first

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", require("./routers"));
 
 app.listen(port, () => {
